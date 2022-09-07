07eae88625f8503a673d51cc985129e714abe988
steelworks <graham@steelworks.org.uk>
2022-09-07 16:26:17 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-cacpst
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-cacpst)
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MaterialExampleModule} from '../material.module';
+import {CdkVirtualScrollOverviewExample} from './cdk-virtual-scroll-overview-example';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {HttpClientModule} from '@angular/common/http';
+
+@NgModule({
+  declarations: [CdkVirtualScrollOverviewExample],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    MaterialExampleModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [CdkVirtualScrollOverviewExample],
+})
+export class AppModule {}
